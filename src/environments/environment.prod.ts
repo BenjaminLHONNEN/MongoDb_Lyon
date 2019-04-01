b1f8af66df29913cd62d637d9a16f68d6a1d390c
Dumont Baptiste <contact@dumontbaptiste.fr>
2019-04-01 10:29:26 +0200

    Ajout du token pour angular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+   mapbox: {
+     accessToken: 'pk.eyJ1IjoiYmFwdGR1IiwiYSI6ImNqdHg4cjVzajI4cmM0ZGxsbXp2OGJwMWkifQ.XjcxMyGNEcB9QjXnu98yyg'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   mapbox: {
+     accessToken: 'pk.eyJ1IjoiYmFwdGR1IiwiYSI6ImNqdHg4cjVzajI4cmM0ZGxsbXp2OGJwMWkifQ.XjcxMyGNEcB9QjXnu98yyg'
+  }
 };
